ae9f875d120256cb467d9e444be0f82968f3e028
diaogg <disi7493@gmail.com>
2024-03-11 19:04:32 +0700

    first commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Tooltip from '../Tooltip/tooltip';
+
+function App () {
+    return (
+        <section>
+            <h1>Tooltips using React Hooks</h1>
+            <Tooltip title="Tooltip on top" position="top">
+                <button>Tooltip on top</button>
+            </Tooltip>
+            <Tooltip title="Tooltip on bottom" position="bottom">
+                <button>Tooltip on Bottom</button>
+            </Tooltip>
+            <Tooltip title="Tooltip on left" position="left">
+                <button>Tooltip on left</button>
+            </Tooltip>
+            <Tooltip title="Tooltip on right" position="right">
+                <button>Tooltip on right</button>
+            </Tooltip>
+        </section>
+    );
+}
+export default App;
